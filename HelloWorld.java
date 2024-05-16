1f67a555cdfe655d6c1c3e249c0d7bbfb56c26c7
lucasabdouni@hotmail.com <lucasabdouni@hotmail.com>
2024-05-16 18:27:47 -0300

    feat: first class java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * HelloWorld = nome da minha classe
+ * public = tipo de acesso da classe
+ * class = tipo da classe
+ */
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
