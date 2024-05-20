b52a1304bfce0af41f69415a216177fc77cc448a
lucasabdouni@hotmail.com <lucasabdouni@hotmail.com>
2024-05-20 19:48:03 -0300

    feat: conditional structure

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,25 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+  
+
+    int dadoDoTipoInt = 10;
+
+    if (dadoDoTipoInt == 10) {
+      System.out.println("Dado do tipo int é igual a 10");
+    } else {
+      System.out.println("Dado do tipo int não é igual a 10");
+    }
+
+    int valorInicial = 0;
+  
+    while(valorInicial < 3) {
+      System.out.println("O valor inicial é menor que 3");
+      valorInicial++;
+    }
+
+    for(int i = 0; i < 4; i++) {
+      System.out.println("O valor de i é: " + i);
+    }
   }
 }
